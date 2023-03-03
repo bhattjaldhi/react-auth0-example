c98272dec54daf25a882b79b545b096b56ba9408
jaldhi <jaldhi@samcomtechnobrins.com>
2023-03-03 18:12:42 -0500

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +16,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        
+        <br /> 
+        
+        {isAuthenticated && <Profile />}
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-k0t0jy7izg0swpbq.us.auth0.com"
+      clientId="NdKRZGMPMAzDowWwt13k3biQiMBcdErs"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
